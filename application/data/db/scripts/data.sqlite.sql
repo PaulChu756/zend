a5fbd1470a9bc737e430d8e854143d8b1b89ce54
PaulChu <PaulChu>
2016-11-30 16:19:29 -0600

    added pdo database

---
+++ application/data/db/scripts/data.sqlite.sql
@@ -0,0 +1,4 @@
+INSERT INTO guestbook(email, comment, created)
+    VALUES ('ralph@zend.com', 'blah ZFAPP', DATETIME('NOW'));
+INSERT INTO guestbook(email, comment, created)
+    VALUES ('pandalord@gmail.com', 'pandalord', DATETIME('NEVER'));
--- application/data/db/scripts/schema.sqlite.sql
+++ application/data/db/scripts/schema.sqlite.sql
@@ -1,3 +1,8 @@
 CREATE TABLE guestbook (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-)
+    email VARCHAR(32) NOT NULL DEFAULT 'noemail@test.com',
+    comment TEXT NULL,
+    create DATETIME NOT NULL
+);
+
+CREATE INDEX "id" on "guestbook" ("id");
